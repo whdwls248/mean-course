194c00eff9193a435b12b7a146bf26d8e1732836
whdwls248 <dmlrud1005@naver.com>
2024-06-20 15:53:42 +0900

    material 적용

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-post-create',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, MatInputModule, MatCardModule, MatButtonModule],
   templateUrl: './post-create.component.html',
   styleUrl: './post-create.component.css'
 })

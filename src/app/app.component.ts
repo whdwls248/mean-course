9c50f5c433ef1f62660ab47c2e5d283fa4df2c3d
whdwls248 <dmlrud1005@naver.com>
2024-06-20 16:02:38 +0900

    header 추가

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms'
 import { PostCreateComponent } from "./post/post-create/post-create.component";
+import { HeaderComponent } from './header/header.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, PostCreateComponent, FormsModule]
+    imports: [RouterOutlet, PostCreateComponent, HeaderComponent, FormsModule]
 })
 export class AppComponent {
   title = 'mean-course';
